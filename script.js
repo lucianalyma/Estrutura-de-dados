cb7ed79e369765d01f153d94176c0a8b38433f03
lucianalyma <lu_llim@hotmail.com>
2022-06-10 19:28:49 -0300

    dados

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Luciana";
+console.log (nome);
+let ano1 = 2;
+let ano2 = 2022;
+console.log (ano1-ano2);
+let texto = "Estou mexendo no console.log";
+console.log (texto);
+let quartaFeira = true;
+console.log (quartaFeira);
